015b62303d4b5933a214c8971702750e62318914
Grady Whelan <32005439+gradyw@users.noreply.github.com>
2021-02-13 11:18:24 -0800

    add backend file to deal with DB

---
+++ Backend/Parse/backend.js
@@ -0,0 +1,15 @@
+const Parse = require('parse/node')
+
+Parse.initialize(/* APP ID*/)
+Parse.serverURL = 'http://localhost:1337/parse'
+
+
+/**
+ * Needs to:
+ * 1. 
+ * 
+ */
+
+async function run() {
+    
+}
