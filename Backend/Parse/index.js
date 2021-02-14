2044d95298ca7b66f4994010e5bd6bf5db7111a2
Grady Whelan <32005439+gradyw@users.noreply.github.com>
2021-02-13 23:57:02 -0800

    remove cloud stuff

--- Backend/Parse/index.js
+++ Backend/Parse/index.js
@@ -72,14 +72,13 @@ async function giveMessageToUser() {
 // addUserToMessage("ZtQfcIwfl6", "aaaaaaxxxxj")
 
 // const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-const databaseUri = process.env.DATABSE_URI;
+const databaseUri = process.env.DATABASE_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'app',
   masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
