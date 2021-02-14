db69a9187a06f9209793038f072bb0ed94722e15
danieltsai-RSheepDV <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2021-02-13 17:02:38 -0800

    changed to fetch communication

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,28 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-import { AsyncStorage } from "react-native";
-var Parse = require("parse/react-native");
+import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>Messege <code>this.state.apiResponse</code> !!</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
